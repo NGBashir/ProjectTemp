d1c27070eb0f80eb1628586dec397ec90cdf7d36
NG <e.bashir@mdx.ac.ae>
2023-11-28 09:52:46 +0400

    created a module for temp and main program with user input

---
+++ checktemp.py
@@ -0,0 +1,7 @@
+import mytemp as t
+c=int (input("ehter you temp in C and we will provide thFH temp"))
+
+print(c, "celcuis is equivlent to ", t.celToFeh(c))
+
+f=int (input("ehter you temp in FH and we will provide the C temp"))
+print(f, "celcuis is equivlent to ", t.fehToCel(f))
---
+++ mytemp.py
@@ -0,0 +1,6 @@
+def celToFeh (ctemp):
+    ftemp=9/5*ctemp+32
+    return ftemp
+def fehToCel(ftemp):
+    ctemp=(ftemp-32)*5/9
+    return ctemp
